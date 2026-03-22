4635d83fb20a2932303200f3046e1b93aeed12e7
seria <seria.ati@gmail.com>
2026-03-22 23:28:38 +0800

    Add site production URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://ef.seria.moe",
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "zh-CN", "zh-TW"],
+        routing: {
+            prefixDefaultLocale: false,
+            fallbackType: "redirect",
+        },
+    },
     vite: {
         plugins: [tailwindcss()],
     },
